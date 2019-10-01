f0e9f534bae708695e37109fa1c552ecfc906393
ericbanker12 <ericbanker12@gmail.com>
2019-10-01 14:31:30 -0500

    get userdata on mount

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
+import './App.css'
+
+// import Card from './components/Card'
 
 class App extends React.Component {
+    state = {
+        userName: 'ericbanker12',
+        userData: {},
+        userFollowers: [],
+    }
+
+    componentDidMount() {
+        axios.get(`https://api.github.com/users/${this.state.userName}`)
+            .then(resp => {
+                this.setState({
+                    userData: resp.data
+                })
+            })
+            .catch(console.error)
+    }
+
     render() {
         return (
             <div className="App">
@@ -11,4 +30,4 @@ class App extends React.Component {
     }
 }
 
-export default App;
+export default App
