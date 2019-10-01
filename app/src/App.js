deafd8f35250ac1568109a21150c2e12c9a42832
ericbanker12 <ericbanker12@gmail.com>
2019-10-01 14:45:25 -0500

    create Card component, display userData

--- app/src/App.js
+++ app/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import axios from 'axios'
 import './App.css'
 
-// import Card from './components/Card'
+import Card from './components/Card'
 
 class App extends React.Component {
     state = {
@@ -24,7 +24,7 @@ class App extends React.Component {
     render() {
         return (
             <div className="App">
-                
+                <Card {...this.state.userData} />
             </div>
         );
     }
---
+++ app/src/components/Card.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Card(props) {
+    return (
+        <div>
+            <img src={props.avatar_url} alt={props.name} />
+            <h3>{props.name || props.login}</h3>
+            <p>{props.login}</p>
+            <p>Location: {props.location || 'N.A.'}</p>
+            <p>Profile:  
+                <a href={props.html_url}>{props.html_url}</a>
+            </p>
+            <p>Followers: {props.followers}</p>
+            <p>Following: {props.following}</p>
+            <p>Bio: {props.bio || 'N.A.'}</p>
+        </div>
+    )
+}
+
+export default Card
