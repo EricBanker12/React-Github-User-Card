2a57b182b703279a498cd746a4643bd9f40a2c6b
ericbanker12 <ericbanker12@gmail.com>
2019-10-01 16:53:22 -0500

    add materialize dependency, style Card component, add userFollowers list and page number to state

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import axios from 'axios'
+import 'materialize-css/dist/css/materialize.min.css'
 import './App.css'
 
 import Card from './components/Card'
@@ -9,6 +10,8 @@ class App extends React.Component {
         userName: 'ericbanker12',
         userData: {},
         userFollowers: [],
+        userFollowersData: [],
+        page: 1,
     }
 
     componentDidMount() {
@@ -22,21 +25,28 @@ class App extends React.Component {
     }
 
     componentDidUpdate(prevProps, prevState) {
+        console.log('componentDidUpdate')
         // if different user, get followers
         if (this.state.userData.followers_url && (!prevState.userData.followers_url || prevState.userData.followers_url !== this.state.userData.followers_url)) {
+            console.log('new user')
             // clear previous user followers
             this.setState({
-                userFollowers: []
+                userFollowers: [],
+                userFollowersData: [],
+                page: 1,
             })
             // get followers
             axios.get(this.state.userData.followers_url)
                 .then(resp => {
-                    resp.data.slice(0,3).forEach(follower => {
+                    this.setState({
+                        userFollowers: resp.data.map(e => e.login)
+                    })
+                    this.state.userFollowers.slice(0,3).forEach(follower => {
                         // get follower's data
-                        axios.get(`https://api.github.com/users/${follower.login}`)
+                        axios.get(`https://api.github.com/users/${follower}`)
                             .then(resp => {
                                 this.setState({
-                                    userFollowers: [...this.state.userFollowers, resp.data]
+                                    userFollowersData: [...this.state.userFollowersData, resp.data]
                                 })
                             })
                             .catch(console.error)
@@ -44,13 +54,18 @@ class App extends React.Component {
                 })
                 .catch(console.error)
         }
+        else if (this.state.page !== prevState.page && this.state.userFollowers[0] && this.state.userFollowersData[0]) {
+            console.log('new page')
+            // do stuff
+        }
     }
 
     render() {
         return (
             <div className="App">
                 <Card {...this.state.userData} />
-                {this.state.userFollowers.map(follower => <Card {...follower} />)}
+                <h4>Followers:</h4>
+                {this.state.userFollowersData.map(follower => <Card key={follower.id} {...follower} />)}
             </div>
         );
     }
--- app/src/components/Card.js
+++ app/src/components/Card.js
@@ -2,17 +2,27 @@ import React from 'react'
 
 function Card(props) {
     return (
-        <div>
-            <img src={props.avatar_url} alt={props.name} />
-            <h3>{props.name || props.login}</h3>
-            <p>{props.login}</p>
-            <p>Location: {props.location || 'N.A.'}</p>
-            <p>Profile:&nbsp;
-                <a href={props.html_url}>{props.html_url}</a>
-            </p>
-            <p>Followers: {props.followers}</p>
-            <p>Following: {props.following}</p>
-            <p>Bio: {props.bio || 'N.A.'}</p>
+        <div className="row">
+            <div className="col m2"></div>
+            <div className="col s12 m8 card horizontal">
+                <div className="card-image">
+                    <img src={props.avatar_url} alt={props.name} />
+                </div>
+                <div className="card-stacked">
+                    <div className="card-content">
+                        <span className="card-title">{props.name || props.login}</span>
+                        <p>{props.login}</p>
+                        <p>Location: {props.location || 'N.A.'}</p>
+                        <p>Followers: {props.followers}</p>
+                        <p>Following: {props.following}</p>
+                        <p>Bio: {props.bio || 'N.A.'}</p>
+                    </div>
+                    <div className="card-action center-align">
+                        <a href={props.html_url}>Profile</a>
+                        {props.blog?<a href={props.blog}>Website</a>:''}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
